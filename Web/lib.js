06e76aa994bb957238519e4fe23dc740382271ef
TheTrueProblematic <mwmttlgmail.com>
2021-02-11 20:27:16 -0700

    WebWork

--- Web/lib.js
+++ Web/lib.js
@@ -1,11 +1,15 @@
-    function signIn(){
-        window.location='http://www.google.com';
-    }
-    function homePage(){
-        console.log("HELLO");
-        window.location='landing.html';
-    }
-    function repair(){
-        console.log("Repair request form opened");
-        window.location='https://docs.google.com/forms/d/e/1FAIpQLSepG2hFEUdOpcTV3EpBfJyftrWDuWXSjow9XU39r8SxGvMl0Q/viewform?usp=sf_link';
-    }
+function signIn(){
+    var uname = btoa(document.getElementById("user").value);
+    var passw = btoa(document.getElementById("pass").value);
+    if(uname=="VXNlcm5hbWU="&passw=="UGFzc3dvcmQ="){
+    window.location='landing.html';
+  }
+}
+function homePage(){
+    console.log("HELLO");
+    window.location='landing.html';
+}
+function repair(){
+    console.log("Repair request form opened");
+    window.location='https://docs.google.com/forms/d/e/1FAIpQLSepG2hFEUdOpcTV3EpBfJyftrWDuWXSjow9XU39r8SxGvMl0Q/viewform?usp=sf_link';
+}
