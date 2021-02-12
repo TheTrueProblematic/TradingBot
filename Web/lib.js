e3c6357d3af846bcba946c9247c2bfe9bfe9e76c
TheTrueProblematic <mwmttlgmail.com>
2021-02-12 01:19:53 -0700

    cookies?

--- Web/lib.js
+++ Web/lib.js
@@ -14,6 +14,14 @@ function repair(){
     window.location='https://docs.google.com/forms/d/e/1FAIpQLSepG2hFEUdOpcTV3EpBfJyftrWDuWXSjow9XU39r8SxGvMl0Q/viewform?usp=sf_link';
 }
 
+function check(){
+  var mode = getCookie("mode");
+  alert(mode);
+  if(mode=="dark"){
+    darkMode();
+  }
+}
+
 function darkMode(){
   document.getElementById('seeinlight').style.right = '-100vw';
   document.getElementById('seeindark').style.right = '2vw';
@@ -22,6 +30,7 @@ function darkMode(){
   document.getElementById('h1text').style.color="#000000";
   document.getElementById('h1text').style.textShadow="1px 1px white";
   document.getElementById('body').style.color="#FAFAFF";
+  setCookie("mode", "dark", 30);
 }
 function lightMode(){
   document.getElementById('seeindark').style.right = '-100vw';
@@ -31,4 +40,26 @@ function lightMode(){
   document.getElementById('h1text').style.color="#FAFAFF";
   document.getElementById('h1text').style.textShadow="2px 2px black";
   document.getElementById('body').style.color="#000000";
+  setCookie("mode", "light", 30);
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
