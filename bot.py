e0e4d6423d5ddfa90b09cbd5e9637a1fc339be68
TheTrueProblematic <mwmttl@gmail.com>
2021-02-13 02:04:44 -0700

    Output to Files

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import shrimpy
 import time
 from os import name
 from subprocess import call
+import sys
 
 
 class bot:
@@ -10,8 +11,17 @@ class bot:
 
 
 class printBot(bot):
+    def __init__(self):
+        self.stream = False
+        self.clearLog()
+
     def clear(self):
         _ = call('clear' if name == 'posix' else 'cls')
+        self.logWrite('output.txt', '\n\n\n')
+        self.logWrite('log.txt', '\n\n\n')
+
+    def error_handler(self, err):
+        self.print("Error", err)
 
     def titleCenter(self, text):
         l = len(text)
@@ -46,15 +56,49 @@ class printBot(bot):
 
             return ret
 
+    def clearLog(self):
+        file = open("output.txt", "r+")
+        file.truncate(0)
+        file.close()
+
+    def logWrite(self, filename, content):
+        # import sys
+        # print('This message will be displayed on the screen.')
+        original_stdout = sys.stdout # Save a reference to the original standard output
+        with open(str(filename), 'w') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            print(str(content)) #This message will be written to a file
+            sys.stdout = original_stdout # Reset the standard output to its original value
+
+    def fullPrint(self, content):
+        self.logWrite('output.txt', content)
+        self.logWrite('log.txt', content)
+        print(str(content))
+
     def print(self, title, content):
+        if self.stream:
+            self.fullPrint(str(title)+": "+str(content))
+        else:
+            self.clear()
+            time.sleep(1)
+            full = self.titleCenter(str(title)) + "\n\n" + self.wrap(str(content))
+            self.fullPrint(full)
+
+    def startPrintStream(self):
         self.clear()
         time.sleep(1)
-        print(self.titleCenter(title) + "\n")
-        print(self.wrap(content))
+        self.stream = True
+
+    def endPrintStream(self):
+        self.clear()
+        time.sleep(1)
+        self.stream = False
+
 
 
 class pumpBot(printBot):
     def __init__(self, exchange_public_key,exchange_secret_key, shrimpy_public_key, shrimpy_secret_key, user_id, account_id):
+        super().__init__()
         self.ex_pub = exchange_public_key
         self.ex_sec = exchange_secret_key
         self.smp_pub = shrimpy_public_key
@@ -91,3 +135,12 @@ class pumpBot(printBot):
 
     def limitSell(self, coinFrom, coinTo, limit):
         return coinFrom
+
+    def fileCoin(self):
+        f = open("coin.txt", "r")
+        content = f.read()
+        l = len(content)
+        if l>0:
+            return content
+        else:
+            return None
